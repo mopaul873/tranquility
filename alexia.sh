84ddccf4e360064cb7791da68311c4e1e280fc7d
Paul Momo <mpny98@yahoo.com>
2024-02-19 05:58:48 -0500

    Alexia the warrior

---
+++ alexia.sh
@@ -0,0 +1,29 @@
+
+#!/bin/bash
+
+# Description: Git repo and bash shell revision
+# Author: Kate Honey
+# Date: February 19 2024
+
+echo "The happyest man on earth is me"
+
+AGE=50
+echo  "i am $AGE years old"
+echo "Display the number of cpu"
+sleep 3
+nproc
+echo "display the memory information"
+sleep 3
+free -m
+echo "Display the kernel information"
+sleep 3
+uname -r
+echo "print the 2024 calendar"
+sleep 3
+cal 2024
+echo "display the disk  info"
+sleep 3
+df -h
+echo "print all the command used in the  current session"
+sleep 3
+history
