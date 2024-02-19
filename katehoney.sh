94d10a1c69e68573eb2d8a7f0eea777f4f07a17c
Paul Momo <mpny98@yahoo.com>
2024-02-19 06:08:06 -0500

    mother of the land

---
+++ katehoney.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Description: The mother of the land
+# Author:  Princeton Jayden Jukane-momo
+# Date: February 19 2024
+
+
+echo  " dear mom. where will this family be, without you? Thank  you for not giving up despite all the odds against you"
