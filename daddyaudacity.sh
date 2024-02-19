21e3485ca225e0bb34837712a1a621bd801bbf4a
Paul Momo <mpny98@yahoo.com>
2024-02-19 06:41:30 -0500

    daddyMo addition

---
+++ daddyaudacity.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Description: The Momo's in full display: 2024-2025 the Glory years begins......
+# Author:daddy Paul and mamma Carole
+# Date: February 19 2024
+
+NAME=Momo
+echo " This is a bash shell environment with Mr $NAME"
+echo "$NAME is one of thr most fearless and audacious man to ever live"
+echo " The $NAME family will be in full display soon"
+echo " It is an honor and privilege to be a $NAME"
